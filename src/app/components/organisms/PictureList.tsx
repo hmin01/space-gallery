f819915e1dde738099e566ba673d901b2bdb42a1
hmin01 <goals1003@gmail.com>
2023-07-06 10:34:09 +0900

    2023.07.06 - 빌드 오류 수정

--- src/app/components/organisms/PictureList.tsx
+++ src/app/components/organisms/PictureList.tsx
@@ -8,8 +8,8 @@ export default function PictureList({ items }: { items: PictureInfo[] }): JSX.El
   return (
     <div className="gap-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6">
       {items.map((info: PictureInfo): React.ReactNode => (
-        <ClickableBox info={info}>
-          <PictureCard key={info.id} title={info.title} src={info.url} />
+        <ClickableBox key={info.id} info={info}>
+          <PictureCard title={info.title} src={info.url} />
         </ClickableBox>
       ))}
     </div>
