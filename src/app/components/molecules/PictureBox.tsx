6070fb8b661b49e42e6ff05f2d0769030f2e4b52
hmin01 <goals1003@gmail.com>
2023-07-07 10:29:33 +0900

    2023.07.07 - 갤러리(= 사진 목록) 페이지 반응형 UI 수정

--- src/app/components/molecules/PictureBox.tsx
+++ src/app/components/molecules/PictureBox.tsx
@@ -4,8 +4,8 @@ import PictureCover from "../atoms/PictureCover";
 
 export default function PictureBox({ blurDataURL, title, src }: { blurDataURL?: string, title?: string, src: string }): JSX.Element {
   return (
-    <div className="overflow-hidden relative rounded-xl">
-      <Picture blurDataURL={blurDataURL} src={src} />
+    <div className="overflow-hidden relative rounded-xl ring-1 ring-zinc-800">
+      <Picture blurDataURL={blurDataURL} ratio={9 / 6} src={src} />
       <PictureCover title={title} />
     </div>
   );
--- src/app/components/organisms/PictureList.tsx
+++ src/app/components/organisms/PictureList.tsx
@@ -6,7 +6,7 @@ import type { PictureInfo } from "../../../../servers/types/picture";
 
 export default function PictureList({ items }: { items: PictureInfo[] }): JSX.Element {
   return (
-    <div className="gap-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6">
+    <div className="gap-4 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 max-w-[1960px] mx-auto">
       {items.map((info: PictureInfo): React.ReactNode => (
         <ClickableBox key={info.id} info={info}>
           <PictureBox blurDataURL={info.dataUrl} title={info.title} src={info.url} />
