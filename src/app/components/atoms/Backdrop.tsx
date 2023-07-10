45c840f9fcdb3f01e046dc4e6052e9a8d8790144
hmin01 <goals1003@gmail.com>
2023-07-10 14:26:43 +0900

    2023.07.10 - Backdrop 배경색이 어색한 문제 해결 - 터치 이벤트 추가 - IOS screen height 문제 해결을 위한 스타일 적용

--- src/app/components/atoms/Backdrop.tsx
+++ src/app/components/atoms/Backdrop.tsx
@@ -5,9 +5,12 @@ import Link from "next/link";
 export default function Backdrop({ background, children }: { background?: string, children?: React.ReactNode }): JSX.Element {
   return (
     <div className="flex h-full items-center justify-center overflow-hidden relative">
-      <Link className="absolute backdrop-blur-2xl h-full top-0 w-full z-10" href="/">
+      <Link className="absolute h-full top-0 w-full z-10" href="/">
         {background ? (
-          <Image alt="backdroup" className="pointer-events-none" fill src={background} />
+          <>
+            <Image alt="backdroup" className="pointer-events-none" fill src={background} />
+            <div className="absolute backdrop-blur-lg bg-black bg-opacity-30 h-full top-0 w-full z-10"></div>
+          </>
         ) : (
           <div className="bg-black"></div>
         )}
